d9285039313a05eb9d675cae2499950f29e0800f
yujinsong-sub <yujinsong.sw@gmail.com>
2020-12-29 02:08:11 +0900

    blog view and template updated

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # 3번째 매개변수 -> posts(Post 객체) 전달
+    return render(request, 'blog/post_list.html', {'posts' : posts})
